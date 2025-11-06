dc131f7ad4f8a65423e31f883aa2b435bbf65d24
mahmoudhelal310-dotcom <mahmoudhelal310@gmail.com>
2025-11-06 09:48:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const searchInput = document.querySelector(".menu input");
+const menuItems = document.querySelectorAll(".menu li");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+  menuItems.forEach(item => {
+    item.style.display = item.textContent.toLowerCase().includes(value) ? "" : "none";
+  });
+});
 
